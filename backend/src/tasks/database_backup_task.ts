d449d7eeb05236b184cc5e097fa0578744cb329e
MashodRana <mashod0rana@gmail.com>
2023-12-06 00:53:18 +0600

    [ADDED] old dumped db deletion

--- backend/src/tasks/database_backup_task.ts
+++ backend/src/tasks/database_backup_task.ts
@@ -1,17 +1,35 @@
 import process from "child_process";
-const cron= require("node-cron");
 
 import config from "../config";
+
+const cron = require("node-cron");
+const fs = require("fs");
+const path = require("path");
+
 
 
 const backupDatabase = () => {
     const todayDate = new Date().toISOString().slice(0, 10);
     const timestamp = Date.now();
-    const backupDbName = `${config.db_name}-${todayDate}-${timestamp}.dump`;
-    const COMMAND = `docker exec ${config.db_container_name} pg_dump -U ${config.db_user} -Fc ${config.db_name} > ${config.db_backup_path}${backupDbName}`;
+    const backupDbName: string = `${config.db_name}-${todayDate}-${timestamp}.dump`;
+    const toBackupDbPath: string = path.join(config.db_backup_path, backupDbName);
+
+    const COMMAND: string = `docker exec ${config.db_container_name} pg_dump -U ${config.db_user} -Fc ${config.db_name} > ${toBackupDbPath}`;
     try {
+
+        // dump the database 
         process.exec(COMMAND)
-        console.log(`Database backup successfull file file ${backupDbName}`);
+        console.log(`Database backup successfull for the file ${backupDbName}`);
+
+        // Delete old dumped database
+        const oldFileNames = fs.readdirSync(config.db_backup_path);
+        oldFileNames.forEach((fileName: string) => {
+            if (fileName !== backupDbName) {
+                // Delete file
+                const filePath: string = path.join(config.db_backup_path, fileName)
+                fs.unlinkSync(filePath);
+            }
+        });
     }
     catch (err) {
         console.log(err)
